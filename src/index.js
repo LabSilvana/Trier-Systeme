17c46e8693ac9e0330827bdbab873b73e2e06c65
Silvana <silvana15_91@hotmail.com>
2020-01-23 20:20:39 -0600

    Se Agrego users, models y controllers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const bodyParser = require('body-parser');
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+const usersRoutes = require('./routes/users');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://trier-systeme:1qa2ws3ed@cluster0-lyx8g.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(db => console.log('db is connected'))
+  .catch(err => console.log(err))
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
 
 app.listen(app.get('port'), () => {
     console.log('server on port ', app.get('port'))
